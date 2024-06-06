5b7f34c6c254f3b028326aea9099182466fd5419
Aniket Naik <aniketn3@gmail.com>
2024-06-06 19:45:34 +0900

    Minor style changes

--- src/modules/main/items.js
+++ src/modules/main/items.js
@@ -1,8 +1,8 @@
 export default [
   {id: "4", name: "Practice Sketching", type: "toast"},
   {id: "0", name: "Learn Japanese", type: "info"},
-  {id: "1", name: "Play guitar", type: "success"},
+  {id: "1", name: "Work out", type: "success"},
   {id: "2", name: "Practice LD", type: "warn"},
   {id: "3", name: "Practice Sketching", type: "error"},
-  {id: "5", name: "Practice Sketching", type: "blurb"}
+  {id: "5", name: "Practice Kanji", type: "blurb"}
 ];
